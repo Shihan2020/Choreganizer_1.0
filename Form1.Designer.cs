e022385efcf5eeaf238413035c59edb469035219
Shihan2020 <68342487+Shihan2020@users.noreply.github.com>
2020-07-24 08:37:34 -0400

    Save to database!!!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -194,6 +194,7 @@
             this.button1.TabIndex = 10;
             this.button1.Text = "SAVE";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label4
             // 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -36,6 +37,32 @@ namespace Choreganizer
             this.usersTableAdapter.Fill(this.choreganizer_ARDataSet.users);
             // TODO: This line of code loads data into the 'choreganizer_ARDataSet.chores' table. You can move, or remove it, as needed.
             this.choresTableAdapter.Fill(this.choreganizer_ARDataSet.chores);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // CREATED 7/22/20 
+            // added code based on ProgrammingKnowledge: https://www.youtube.com/watch?v=sSBx9_eddmo
+
+            string conString = "Data Source=choreganizerdb.cg1zijglf85b.us-east-2.rds.amazonaws.com,1433;Initial Catalog=choreganizer_AR;" +
+                "Persist Security Info=True;User ID=shihan2020;Password=Psycobetabuckdown";
+            string Query = "INSERT INTO [dbo].[history] (chore_id, user_id) VALUES ('" + this.comboBox2.Text + "'," +
+                "'" + this.comboBox1.Text + "'); ";
+            SqlConnection conSaveChore = new SqlConnection(conString);
+            SqlCommand cmdSaveChore = new SqlCommand(Query, conSaveChore);
+            SqlDataReader myReader;
+            try
+            {
+                conSaveChore.Open();
+                myReader = cmdSaveChore.ExecuteReader();
+                MessageBox.Show("Saved!!");
+                while (myReader.Read()) { }
+            } 
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
     }
